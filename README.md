9ceaa7248b44affe600fe8e1d6ceb7196e258d15
vg101 <virtualguard101@gmail.com>
2026-04-29 09:30:33 +0800

    chore: add a bootstrap script

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # vglab
 
 Lab repo for Computer Science self learning. 
+
+## Getted Started
+
+- Clone the repo with submodules
+
+```bash
+git clone --recurse-submodules git@github.com:virtualguard101/vglab.git
+```
+
+- Set up the environment
+
+```bash
+./bootstrap.sh
+```
---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -uoe
+
+# check and install infrastructure
+if ! command -v uv &> /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    echo 'uv installed successfully'
+fi
+
+if ! command -v cargo &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo 'cargo installed successfully'
+fi
+
+if ! command -v just &> /dev/null; then
+    cargo install just
+    echo 'just installed successfully'
+fi
+
+# install dependencies
+uv sync
+echo 'dependencies installed successfully'
+
+# check whether the machine has a GPU
